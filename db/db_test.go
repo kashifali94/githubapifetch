c6d0fd8d7edd60ee360ad4c7c6b8a5fd753b13ff
Kashif Ali <kash_khan422@yahoo.com>
2025-06-06 07:05:41 +0500

    Update db_test.go

--- db/db_test.go
+++ db/db_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"githubassign/models"
+	"githubapifetch/models"
 )
 
 // setupTestDB creates a new test database connection with a mock
